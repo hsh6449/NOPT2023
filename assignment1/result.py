9a77e8c9c3ed9ecc2c1acf3a725b724bbd31e3a3
sanha Hwang <sanhahwang@192.168.nate.com>
2023-09-16 23:22:02 +0900

    final code

--- assignment1/result.py
+++ assignment1/result.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import rootfinding as rf
 
+import numpy as np
+
 f1 = lambda x: x**3 - 3*x**2 + 3*x - 1
 f2 = lambda x: x**5 - 4*x**4 + 3*x**3 + 9*x - 5
+f3 = lambda x: x**3 + np.cos(x) # a = -1, b = 0
 
 b_root, b_a, b_b = rf.bisection(f2, -15, 17)
+n_root = rf.newton(f2, 3)
+s_root = rf.secant(f2, -10, 15)
+r_root = rf.regular_falsi(f2, -10, 15)
 
 print(f"Bisection : root - {b_root:.3f}, [{b_a}, {b_b}]")
+print(f"Newton : root - {n_root:.3f}")
+print(f"Secant : root - {s_root:.3f}")
+print(f"Regular Falsi : root - {r_root:.3f}")
--- assignment1/rootfinding.py
+++ assignment1/rootfinding.py
@@ -9,10 +9,16 @@ def bisection(f, a, b, tol=1e-6):
 
   global ITER
   global MAXITER
+
+
+  if ITER == 0 :
+    print("-------------------------Bisection Method-------------------------")
+    print(f"[Starting Point] - [a] : {a}, [b] : {b} \n")
+  
   start = time.time()
 
   if ITER == MAXITER:
-      return a, b
+      return (a+b)/2, a, b
   
   fx = f((a+b)/2)
 
@@ -21,7 +27,7 @@ def bisection(f, a, b, tol=1e-6):
     end = time.time()
 
     print("=========================Root Found!=========================")
-    print(f"[terminal interval] - [iter] : {ITER}, [a] : {a}, [b] : {b}, time : {end-start:.9f} sec")
+    print(f"[Terminal Result] - [iter] : {ITER}, [a] : {a}, [b] : {b}, [time] : {end-start:.6f} sec")
 
     return (a+b)/2, a, b
   
@@ -38,14 +44,107 @@ def bisection(f, a, b, tol=1e-6):
     return bisection(f, a, b, tol)
 
 
-def newton(f, df, x0, tol=1e-6):
+def newton(f, x0, tol=1e-5):
 
   global ITER
   global MAXITER
-  pass
 
-def secant(f, x0, x1, tol=1e-6, maxiter=100):
-  pass
+  if ITER is not 0 :
+    ITER = 0
 
-def regular_falsi(f, a, b, tol=1e-6, maxiter=100):
-  pass
+  h = 1e-9
+  old_x = x0
+
+  print("-------------------------Newton Method-------------------------")
+  print(f"[Starting Point] - [X] : {old_x} \n")
+  start = time.time()
+
+  while ITER < MAXITER:
+
+    fx = f(old_x)
+    dfx = (f(old_x + h) - f(old_x)) / h
+
+    new_x = old_x - fx/dfx
+
+    ITER += 1
+    print(f"[Root Finding...] - [iter] : {ITER}, [X] : {new_x} ")
+
+    if (abs(new_x - old_x)) < tol:
+      break
+
+    old_x = new_x
+
+  end = time.time()
+  print("=========================Root Found!=========================")
+  print(f"[Terminal Result] - [iter] : {ITER}, [X] : {new_x}, [time] : {end - start:.5f} sec\n")
+  return new_x
+
+
+def secant(f, x0, x1, tol=1e-6):
+
+  global ITER
+  global MAXITER
+
+  if ITER is not 0 :
+    ITER = 0
+
+  print("-------------------------Secant Method-------------------------")
+  print(f"[Starting Point] - [X0] : {x0}, [X1] : {x1} \n")
+
+  start = time.time()
+
+  while ITER < MAXITER:
+      
+      fx0 = f(x0)
+      fx1 = f(x1)
+  
+      new_x = x1 - fx1 * ((x1 - x0) / (fx1 - fx0))
+  
+      ITER += 1
+      print(f"[Root Finding...] - [iter] : {ITER}, [X] : {new_x} ")
+  
+      if (abs(new_x - x1)) < tol:
+        break
+  
+      x0 = x1
+      x1 = new_x
+
+  end = time.time()
+  print("\n=========================Root Found!=========================")
+  print(f"[Terminal Result] - [iter] : {ITER}, [X] : {new_x}, [time] : {end - start:.5f} sec\n")
+  return new_x
+
+def regular_falsi(f, a, b, tol=1e-6):
+  
+  global ITER
+  global MAXITER
+
+  if ITER is not 0 :
+    ITER = 0
+
+  print("-------------------------Regular Falsi Method-------------------------")
+  print(f"[Starting Point] - [a] : {a}, [b] : {b} \n")
+
+  start = time.time()
+
+  while ITER < MAXITER:
+
+    fa = f(a)
+    fb = f(b)
+
+    new_x = b - fb * ((b - a) / (fb - fa))
+
+    ITER += 1
+    print(f"[Root Finding...] - [iter] : {ITER}, [X] : {new_x} ")
+
+    if (abs(new_x - b)) < tol:
+      break
+
+    a = b
+    b = new_x
+
+  end = time.time()
+  print("\n =========================Root Found!=========================")
+  print(f"[Terminal Result] - [iter] : {ITER}, [X] : {new_x}, [time] : {end - start:.5f} sec\n")
+  return new_x
+
