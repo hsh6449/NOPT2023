fb695d5fe300425992b5592f90b726e718dfe21b
Sanha Hwang <57973170+hsh6449@users.noreply.github.com>
2023-09-26 22:30:08 +0900

    ass2 submmit

--- assignment2/result.py
+++ assignment2/result.py
@@ -3,10 +3,19 @@ import math
 
 from search import *
 
-f1 = lambda x: x ** 2 + 54 / x
-f2 = lambda x: abs(x-0.65)
+f1 = lambda x: x**3 - 3*x**2 + 3*x - 1 # x = 1
+f2 = lambda x: x**5 - 4*x**4 + 3*x**3 + 9*x - 5 # x = 0.536
+f3 = lambda x: np.exp(x) - x**np.exp(1) + 3*x**3 -2*x -5  # x = 1.249 
+f4 = lambda x: -20*x*np.exp(-0.2*np.square(x**2)) -np.exp(0.5*(np.cos(2*np.pi*x) + 1)) + 20 + np.exp(1) # x = 0
 
-a, b = fibonacci(f2, 0, 1, 8)
+# a, b = fibonacci(f4, -5, 5, 15)
+function_list = [f1, f2, f3, f4]
+for f in function_list:
+  print(f"Function : f_{function_list.index(f) + 1}")
+  print("-----------------------------------\n")
+  a, b = fibonacci(f, -5, 5, 15)
 
-print("a = ", a)
-print("b = ", b)
+for f in function_list:
+  print(f"Function : f_{function_list.index(f) + 1}")
+  print("-----------------------------------\n")
+  a, b = golden_section(f4, -5, 5, 1e-5)
--- assignment2/search.py
+++ assignment2/search.py
@@ -8,7 +8,14 @@ def fibonacci(f, a, b, n):
   for i in range(2, n + 1):
     fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2]) # generate fibonacci numbers
 
-  x1 = a + (b - a) * fibonacci_numbers[n - 2] / fibonacci_numbers[n] # choose x1 and x2
+  print("Fibonacci number list : ",fibonacci_numbers)
+  print("-----------------------------------\n")
+
+  print("Start point : [{}, {}]\n".format(a, b))
+
+  start = time.time()
+
+  x1 = a + (b - a) * fibonacci_numbers[n - 2] / fibonacci_numbers[n] # calculate x1 and x2
   x2 = b - (b - a) * fibonacci_numbers[n - 2] / fibonacci_numbers[n]
 
   f1 = f(x1) # calculate f(x1) and f(x2)
@@ -21,38 +28,58 @@ def fibonacci(f, a, b, n):
       f1 = f2
       x2 = b - (b - a) * fibonacci_numbers[n - i - 1] / fibonacci_numbers[n - i]
       f2 = f(x2)
+      
+      print(f"[Root finding...] - iter : {i} - interval : [{a}, {b}]" )
     else:
       b = x2
       x2 = x1
       f2 = f1
       x1 = a + (b - a) * fibonacci_numbers[n - i - 2] / fibonacci_numbers[n - i]
       f1 = f(x1)
+      print(f"[Root finding...] - iter : {i} - interval : [{a}, {b}]" )
+  end = time.time()
+
+  print("-----------------------------------\n")
+  print("End point : [{}, {}]\n".format(a, b))
+  print("Time : ", end - start)
 
   return (a,b)
 
 
 def golden_section(f, a, b, TOL=1e-5):
-  f1 = f(a)
-  f2 = f(b)
 
-  x1 = a + 0.618 * (b - a)
+  print("Golden section method")
+  print("-----------------------------------\n")
+  print("Start point : [{}, {}]\n".format(a, b))
+
+  start = time.time()
+  x1 = a + 0.618 * (b - a) # 0.618 is the golden ratio
   x2 = b - 0.618 * (b - a)
 
   f1 = f(x1)
   f2 = f(x2)
 
+  iter = 0
   while abs(b - a) > TOL:
+    iter += 1
     if f1 > f2:
       a = x1
       x1 = x2
       f1 = f2
       x2 = b - 0.618 * (b - a)
       f2 = f(x2)
+      print(f"[Root finding...] - iter : {iter} - interval : [{a}, {b}]" )
     else:
       b = x2
       x2 = x1
       f2 = f1
       x1 = a + 0.618 * (b - a)
       f1 = f(x1)
+      print(f"[Root finding...] - iter : {iter} - interval : [{a}, {b}]" )
+  
+  end = time.time()
+  print("-----------------------------------\n")
+  print("End point : [{}, {}]\n".format(a, b))
+  print("Time : ", end - start)
 
   return (a,b)
