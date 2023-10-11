88aa36e275249537dd18da7be284b936531b0800
Sanha Hwang <57973170+hsh6449@users.noreply.github.com>
2023-10-11 11:25:38 +0900

    final

--- assignment3/algo.py
+++ assignment3/algo.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-import math
+import time
 
 # function list to dict to list With sorting
 def l2d2l(keys,values):
@@ -18,10 +18,15 @@ def l2d2l(keys,values):
   return list(key), list(value)
 
 
-def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.001, max_iter=10000000):
+def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.01, max_iter=10000000, random_seed=0):
   # step 0 : initialization
+  random.seed(random_seed)
+  start = time.time()
+
   x = []
   y = []
+  coordinates = []
+  results = []
 
   iter = 0
 
@@ -36,7 +41,6 @@ def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.001, max_ite
   values_ = list(sorted_fx_.values())
 
   prev_fx = values_[N]
-  fr = 0
   
   while iter < max_iter:
     if iter >= 1 :
@@ -52,17 +56,10 @@ def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.001, max_ite
 
     fr = f(xr, yr) # evalueate fr at xr, yr
   
-    if  values_[0] <= fr <= values_[N-1] : # f(xr, yr) >= f(x1, y1) and f(xr, yr) <= f(xn, yn)
-      # prev_fx = values_[N]
+    if  values_[0] <= fr <= values_[N-1] : 
 
       keys_[N] = (xr, yr)
       values_[N] = fr
-
-      # keys_, values_ = l2d2l(keys_, values_, N)
-
-      # prev_fx = new_fx
-      # new_fx = fr
-      # print(f"iter  - {iter}, xr, yr : {xr}, {yr}")
 
     elif fr > values_[N-1]: # step 3 : contraction
       if fr < values_[N]:
@@ -78,16 +75,9 @@ def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.001, max_ite
       min_value = np.min([fr, values_[N]])
 
       if fc < min_value:
-        # prev_fx = values_[N]
 
         keys_[N] = (xc, yc)
         values_[N] = fc
-
-        # keys_, values_ = l2d2l(keys_, values_, N)
-
-        # prev_fx = new_fx
-        # new_fx = fc
-        # print(f"iter  - {iter}, xc, yc : {xc}, {yc}")
 
       else: # fc >= min_value, step 4 : shrink
         for i in range(N+1):
@@ -102,44 +92,145 @@ def Nelder_Mead(f, N=3, alpha=0.01, beta=1.5, gamma=0.05, epsilon=0.001, max_ite
       fe = f(xe, ye)
 
       if fe <= fr:
-        # prev_fx = values_[N]
 
         keys_[N] = (xe, ye)
         values_[N] = fe
-        
-        # prev_fx = new_fx
-        # new_fx = fe
-        # print(f"iter  - {iter}, xe, ye : {xe}, {ye}")
-      else:
-        # prev_fx = values_[N]
 
+      else:
+        
         keys_[N] = (xr, yr)
         values_[N] = fr
-
-        # keys_, values_ = l2d2l(keys_, values_, N)
-
-        # prev_fx = new_fx
-        # new_fx = fr
-        # print(f"iter  - {iter}, xr, yr : {xr}, {yr}")
-
+        
     keys_, values_ = l2d2l(keys_, values_)
+
+    coordinates.append(keys_[N])
+    results.append(values_[N])
 
     if abs(prev_fx - values_[N]) < epsilon:
       print(f"Converged after {iter} iterations.")
       break
-      # return keys_[N], values_[N]
+      
         
     prev_fx = values_[N]
-
-  print(f"Reached max iterations without convergence.")
-
-  # return keys_[N], values_[N]
-
-def powell(f, N=10, gamma=0.05, epsilon=0.001, max_iter=10000000):
   
-  p = [(1,0), (0,1)] # initial direction is standard unit vector
+  if iter == max_iter:
+    print(f"Reached max iterations without convergence.\n")
 
-  for k in range(N):
-    pass
+  end = time.time()
+  print(f"Final result : (x, y) : {keys_[N]}, fxy : {values_[N]}\n, time : {end-start}")
 
-  pass
+  return coordinates, results
+
+
+def powell(f, gamma=0.05, epsilon=0.01, max_iter=10000000, random_seed=0):
+
+  random.seed(random_seed)
+
+  x, y = random.randint(-1000, 1000), random.randint(-1000, 1000) # random initial point
+  U = [(1,0), (0,1)] # initial direction is standard unit vector
+
+  iter = 0
+  start = time.time()
+
+  coordinates = []
+  results = []
+
+  prev_fx = f(x, y)
+  P = [(x, y)]
+
+  while iter < max_iter:
+    x_start , y_start = x, y
+
+    coordinates.append((x, y))
+    results.append(prev_fx)
+
+    for u in U:
+      # set the new x,y candidates
+      x_new = x + gamma*u[0]
+      y_new = y + gamma*u[1]
+
+      # fx_new = f(x_new, y_new)
+      
+      gamma_f = lambda gamma : f(x_new + gamma*u[0], y_new + gamma*u[1])
+
+      a, b = Univariate_search(gamma_f, initial_point=0, delta=0.01)
+      optimal_gamma = golden_section(gamma_f, a, b)
+
+      x, y = x_new + optimal_gamma*u[0], y_new + optimal_gamma*u[1]
+
+      P.append((x,y))
+
+    U.pop(0)
+    U.append((x - x_start, y - y_start))
+
+
+    iter += 1
+
+
+    new_fx = f(x, y)
+    print(f"iter  - {iter}, (x, y) : {(x, y)}, fxy : {new_fx}\n")
+
+    # 종료조건
+    if abs(prev_fx - new_fx) < epsilon:
+      print(f"Converged after {iter} iterations.")
+      break
+
+    prev_fx = new_fx
+
+  if iter == max_iter:
+    print(f"Reached max iterations without convergence.\n")
+  
+  end = time.time()
+  print(f"Final result : (x, y) : {(x, y)}, fxy : {f(x, y)}\n, time : {end-start}")
+
+  return coordinates, results
+
+
+def golden_section(f, a, b, TOL=1e-5):
+
+  # assignment 2 에서 사용한 golden section method를 사용하여 powell 방법의 step size를 구하려 함
+  # 코드는 assignment 2에서 가져옴 
+
+  x1 = a + 0.618 * (b - a) # 0.618 is the golden ratio
+  x2 = b - 0.618 * (b - a)
+
+  f1 = f(x1) #c
+  f2 = f(x2) #d
+
+  while abs(b - a) > TOL:
+    if f1 > f2:
+      a = x1
+      x1 = x2
+      f1 = f2
+      x2 = b - 0.618 * (b - a)
+      f2 = f(x2)
+
+    else:
+      b = x2
+      x2 = x1
+      f2 = f1
+      x1 = a + 0.618 * (b - a)
+      f1 = f(x1)
+  
+
+  return (a + b)/2 
+
+def Univariate_search(f, initial_point=0.0, delta=0.01):
+    
+    a, fa = initial_point, f(initial_point)
+    b, fb = a + delta, f(a + delta)
+    
+    if fb > fa:
+        a, b = b, a
+        fa, fb = fb, fa
+        delta = -delta
+
+    while True:
+        c, fc = b + delta, f(b + delta)
+        if fc > fb:
+            if a < c:
+                return (a, c)
+            else:
+                return (c, a)
+        a, fa, b, fb = b, fb, c, fc
+        delta *= 2.0
--- assignment3/result.py
+++ assignment3/result.py
@@ -1,6 +1,6 @@
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
 
 from algo import Nelder_Mead, powell
 
@@ -8,15 +8,49 @@ f1 = lambda x,y : (x+3*y-5)**2 + (3*x + y - 7)**2
 f2 = lambda x,y : 50*(x-y**2)**2 + (1-y)**2
 f3 = lambda x,y : (1.5-x + x*y)**2 + (2.25 - x + x*y**2)**2 + (2.625 - x + x*y**3)**2
 
-def plot(f, x, y, z):
-  pass
 
+def contour_plot(result: list, values: list, jitter_amount=1e-10):
+    plt.style.use('default')
 
-def main():
-  Nelder_Mead(f1)
-  # Nelder_Mead(f2)
-  # Nelder_Mead(f3)
+    x = [i[0] + np.random.uniform(-jitter_amount, jitter_amount) for i in result]
+    y = [i[1] + np.random.uniform(-jitter_amount, jitter_amount) for i in result]
+    z = values
+
+    if len(set(x)) == 1:
+      print("All x values are the same. Cannot plot a contour.")
+      return
+
+    x_range = max(x) - min(x)
+    y_range = max(y) - min(y)
+    xi = np.linspace(min(x) - 0.1 * x_range, max(x) + 0.1 * x_range, 100)
+    yi = np.linspace(min(y) - 0.1 * y_range, max(y) + 0.1 * y_range, 100)
+
+    zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='linear')
+
+    fig, ax = plt.subplots()
+    cp = ax.contour(xi, yi, zi, colors='black')
+    ax.clabel(cp, inline=True, fontsize=10)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.scatter(x, y, color='red', marker='o')  # Mark the original points
+
+    for i in range(1, len(x)):
+      ax.arrow(x[i-1], y[i-1], x[i]-x[i-1], y[i]-y[i-1], head_width=0.5, head_length=1, fc='blue', ec='blue')
+
+    plt.show()
+
+def main(method = "Nelder_Mead", f = f2, random_seed=777):
+  if method == "Nelder_Mead":
+    result, values = Nelder_Mead(f, random_seed= random_seed)
+    contour_plot(result, values)
+  elif method == "powell":
+    result, values = powell(f, random_seed= random_seed)
+    contour_plot(result, values)
+  else:
+    print("No such method")
+    pass
+  
 
 
 if __name__ == '__main__':
-  main()
+  main(method = "Nelder_Mead", f = f2, random_seed=777)
