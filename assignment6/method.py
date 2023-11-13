9f526bd7d69f2b3df33dfe6a3761dc57f80816ed
Sanha Hwang <57973170+hsh6449@users.noreply.github.com>
2023-11-14 01:37:08 +0900

    update

--- assignment6/method.py
+++ assignment6/method.py
@@ -1,46 +1,13 @@
 import math
 import numpy as np
+import pdb
 
 from autograd import elementwise_grad as egrad
 from autograd import jacobian
 
 def Levenberg_Marquardt(f, data, target = 1, tol=1e-6, MaxIter=10000):
+        
     
-        x, y , z = data['x'], data['y'], data['z']
-    
-        if target == 1 :
-            target = data["data1"]
-        elif target == 2 :
-            target = data["data2"]
-    
-        iter = 1
-    
-        J = np.array([[1,0,0],
-                    [0,1,0],
-                    [0,0,1]])
-    
-        P = np.array([1,1,1,0])
-    
-        while iter < MaxIter:
-            r = target - f(x, y, z, P[0], P[1], P[2])
-    
-            if np.linalg.norm(r) < tol:
-                return P
-    
-            J = jacobian(f)(x, y, z, P)
-            J_T = np.transpose(J)
-            J_T_J = np.matmul(J_T, J)
-            J_T_r = np.matmul(J_T, r)
-    
-            P = P + np.matmul(np.linalg.inv(J_T_J + 0.001 * np.identity(3)), J_T_r)
-    
-            iter += 1
-    
-        return P
-
-
-def Gauss_Newton(f, data, target = 1, tol=1e-6, MaxIter=10000):
-
     x, y , z = data['x'], data['y'], data['z']
 
     if target == 1 :
@@ -50,24 +17,67 @@ def Gauss_Newton(f, data, target = 1, tol=1e-6, MaxIter=10000):
 
     iter = 1
 
-    J = np.array([[1,0,0],
-                  [0,1,0],
-                  [0,0,1]])
+    J = np.array([[1,0,0,0],
+                  [0,1,0,0],
+                  [0,0,1,0],
+                  [0,0,0,1]])
 
-    P = np.array([1,1,1])
+    P = np.array([1,1,1,1])
 
     while iter < MaxIter:
-        r = target - f(x, y, z, P)
+        r = target - f(x, y, z, P[0], P[1], P[2])
 
         if np.linalg.norm(r) < tol:
             return P
 
-        J = jacobian(f)(x, y, z, P)
+        J = jacobian(f)(np.array(x), np.array(y), np.array(z), P) 
         J_T = np.transpose(J)
         J_T_J = np.matmul(J_T, J)
         J_T_r = np.matmul(J_T, r)
 
-        P = P + np.matmul(np.linalg.inv(J_T_J), J_T_r)
+        P = P + np.matmul(np.linalg.inv(J_T_J + 0.001 * np.identity(3)), J_T_r)
+
+        iter += 1
+
+    return P
+
+
+def Gauss_Newton(f, data, target_w = 1, tol=1e-6, MaxIter=10000):
+
+    x, y , z = data['x'], data['y'], data['z']
+
+    if target_w == 1 :
+        target = data["data1"]
+    elif target_w == 2 :
+        target = data["data2"]
+
+    iter = 1
+
+    J = np.array([[1,0,0,0],
+                  [0,1,0,0],
+                  [0,0,1,0],
+                  [0,0,0,1]])
+
+    P = np.array([1,1,1,1])
+
+    while iter < MaxIter:
+        f_val = f(x, y, z, P)
+        r = target - f_val
+
+        if np.linalg.norm(r) < tol:
+            return P
+
+        J = jacobian(f)(np.array(x), np.array(y), np.array(z), P) 
+        J_T = np.transpose(J)
+        J_T_J = np.matmul(J_T, J) 
+
+        pdb.set_trace()
+
+        P = P + np.matmul(np.linalg.inv(J_T_J), J_T) @ r
+        rmse = np.sqrt(np.sum(r**2)/len(r))
+
+        print(P)
+        print("loss : " , rmse)
 
         iter += 1
 
--- assignment6/result.py
+++ assignment6/result.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import pdb
 
-data_1 = pd.read_table("Data_for_Homework6/Model1_Data.txt", engine='python') # dataframe
-data_2 = pd.read_table("Data_for_Homework6/Model2_Data.txt", engine='python')
+data_1 = pd.read_table("Data/Model1_Data.txt", engine='python') # dataframe
+data_2 = pd.read_table("Data/Model2_Data.txt", engine='python')
 
 data_1 = delete_nan(data_1)
 data_2 = delete_nan(data_2)
@@ -14,13 +14,13 @@ data_2 = delete_nan(data_2)
 print(data_1)
 print(data_2)
 
-def f1(x,y,z):
+def f1(x,y,z, P):
 
   # initial coefficient setting
-  a = 1 
-  b = 1
-  c = 1
-  d = 0
+  a = P[0]
+  b = P[1]
+  c = P[2]
+  d = P[3]
 
   return a*x + b*y + c*z + d
 
@@ -30,18 +30,13 @@ def f2(x,y,z, P):
   a = P[0]
   b = P[1]
   c = P[2]
-  d = p[4]
+  d = P[3]
 
   return np.exp(((x-a)**2 + (y-b)**2 + (z-c)**2)/d**2)
 
-
-# pdb.set_trace()
-
-
-
 def main():
-  pass
+  Gauss_Newton(f1, data_1, 1)
 
-# if __name__ == "__main__":
-#   main()
+if __name__ == "__main__":
+  main()
 
